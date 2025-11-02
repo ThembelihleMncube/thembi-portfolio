bce5edd0ccd225841f587645e4e2a32390b3afdf
ThembelihleMncube <124764074+ThembelihleMncube@users.noreply.github.com>
2025-11-02 11:06:49 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/thembi-portfolio/',   // <-- REQUIRED for project pages
-  server: { port: 5173 },
-  build: { outDir: 'dist' },
+  base: '/thembi-portfolio/',  // must match your repo name
+  build: { outDir: 'dist' }
 })
